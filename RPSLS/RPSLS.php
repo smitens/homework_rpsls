99a81cb36ca2ff521dc7d8f41a962d1475579a3c
smitens <zane.smite@gmail.com>
2024-05-07 11:02:57 +0300

    Changed file structure

--- RPSLS/RPSLS.php
+++ RPSLS/RPSLS.php
@@ -1,4 +1,5 @@
 <?php
+
 $guess = strtolower(readline("Choose Rock, Paper, Scissors, Lizard, Spock: \n"));
 $elements = [
     "rock" => ["scissor", "lizard"],
@@ -9,7 +10,7 @@ $elements = [
 ];
 $computerElement = array_rand($elements);
 $playerElement = $elements[$guess];
-echo "Computer choose: " .$computerElement . "\n";
+echo "Computer choose: " . $computerElement . "\n";
 if (in_array($computerElement, $playerElement)) {
     echo "You win!\n";
 } else if (in_array($guess, $elements[$computerElement])) {
